3a26c67909fa0e10e26bbf4f86b85ba5ce08c943
AnishGulati <anish.gulati@research.iiit.ac.in>
2019-06-18 08:09:37 +0530

    Update testing script

--- test.sh
+++ test.sh
@@ -1,1 +1,12 @@
-for filename in ../examples/*.sif; do echo $filename; python3 lottie-exporter.py $filename; done
+# $1 should contain the python exporter
+# $2 should contain the directory which contains the example files
+if [ $# -eq 0 ]
+    then 
+        directory="../examples";
+        program="lottie-exporter.py"
+    else
+        directory=$2
+        program=$1
+fi
+full_path="$directory/*.sif"
+for filename in $full_path; do echo $filename; python3 $program $filename; done
